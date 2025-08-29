19f63112f53dc3e078915f8e5a8ed60945d509da
Suvajit Sadhukhan <suvajitss1107@gmail.com>
2025-08-30 01:27:34 +0530

    String

--- StringClass/Palindrome.java
+++ StringClass/Palindrome.java
@@ -1,6 +1,6 @@
 package StringClass;
 
-public class Pallindrom {
+public class Palindrome {
     public static boolean pallindrom(String str) {
         for(int i=0; i<str.length()/2; i++){
             if(str.charAt(i) != str.charAt(str.length()-1-i)){
